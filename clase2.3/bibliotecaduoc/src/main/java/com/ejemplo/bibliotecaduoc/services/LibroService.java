a5e7d219f75da905c6e477500dd738dfcd69fcdf
unknown <SSDD@PNAWSA05LC54424.duoc.acad>
2025-04-15 10:15:19 -0400

    creacion de una apit rest

---
+++ clase2.3/bibliotecaduoc/src/main/java/com/ejemplo/bibliotecaduoc/services/LibroService.java
@@ -0,0 +1,38 @@
+package com.ejemplo.bibliotecaduoc.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ejemplo.bibliotecaduoc.model.Libro;
+import com.ejemplo.bibliotecaduoc.repository.LibroRepository;
+
+@Service
+public class LibroService {
+    @Autowired
+    private LibroRepository libroRepository;
+
+    public List<Libro> getLibros() {
+        return libroRepository.obtenerLibros();
+    }
+
+    public Libro saveLibro(Libro libro) {
+        return libroRepository.guardar(libro);
+    }
+
+    public Libro getLibroId(int id) {
+        return libroRepository.buscarPorId(id);
+    }
+
+    public Libro updateLibro (Libro libro) {
+        return libroRepository.actualizarLibro(libro);
+    }
+
+    public String deleteLibro (int id) {
+
+        libroRepository.eliminar(id);
+        return "Producto Eliminado";
+    }
+
+}
